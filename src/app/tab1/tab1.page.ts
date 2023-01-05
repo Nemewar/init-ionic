5657e1cb7ec00a41c33ea110363ec4cecc652006
Frank Pizarro <frank.pizarro0@gmail.com>
2023-01-05 15:31:01 -0500

    init with ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+    let numero = 10;
+    
+
+    numero = 20;
+
+    numero = numero +10;
+
+    numero = 0;
+
+    console.log(numero)
+
+  }
 
 }
